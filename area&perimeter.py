f2fda511c15c200610c7b183e151b1d25af63f43
ImBlue2104 <neel.rp21@gmail.com>
2025-07-08 16:36:59 -0700

    Add files via upload

--- area&perimeter.py
+++ area&perimeter.py
@@ -8,39 +8,39 @@ class Circle:
     by using the given radius by the user!!!'''
         
     #method to calc circumference
-    def compute_circumference(self, rad):
+    def compute_circumference(self, rad,measure):
         circumference = 2*rad*pi
-        print(f"The circunference of a circle with a radius of {rad}{measure} is {round(circumference, 2)}{measure}")
+        print(f"The circumference of a circle with a radius of {rad}{measure} is {round(circumference, 2)}{measure}.")
 
     #method to calc area
-    def compute_area(self, rad):
+    def compute_area(self, rad, measure):
         area = pi*rad**2
-        print(f"The circunference of a circle with a radius of {rad}{measure} is {round(area, 2)}{measure}")
+        print(f"The area of a circle with a radius of {rad}{measure} is {round(area, 2)}{measure} squared.")
 
 
 print('\n' + Circle.__doc__)
 
 #Choose a measure:
-measure = input("\nChoose a measure for computation; a)cm, b)in, c)m, d)ft: ")
-if measure.strip().lower() == 'a':
-    measure = 'cm'
-elif measure.strip().lower() == 'b':
-    measure = 'in'
-elif measure.strip().lower() == 'c':
-    measure = 'm'
-elif measure.strip().lower() == 'd':
-    measure = 'ft'
-else:
-    print("Invalid Input!")
+measure_dict={'a':'cm',
+             'b':'m',
+             'c':'in',
+             'd':'ft'}
+print(measure_dict)
 
+selection = input("\nChoose the measure's letter for computation: ").strip().lower()
+
+if selection  in measure_dict:
+    measure = measure_dict[selection]
+else:
+    print('Invalid Input')
+    exit()
 #get desired input
-radius = int(input("\nEnter desired radius: "))
+radius = float(input("\nEnter desired radius: "))
 
 #create obj
 Circle1 = Circle()
 
 #Call necessary funcs
-Circle1.compute_circumference(radius)
-Circle1.compute_area(radius)
+Circle1.compute_circumference(radius, measure)
+Circle1.compute_area(radius, measure)
 
-del Circle1
